7348807aa5f54b140a15665fbc2382d47816a044
meyli <meyli@ME>
2025-01-21 21:38:14 +0100

    Vista de el menu bar comun para todas las ventanas

--- CRUD_ClientApp/src/eus/tartanga/crud/exception/package-info.java
+++
@@ -1,6 +0,0 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this templaste file, choose Tools | Templates
- * and open the template in the editor.
- */
-package eus.tartanga.crud.exception;
--- CRUD_ClientApp/src/eus/tartanga/crud/logic/package-info.java
+++
@@ -1,6 +0,0 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change thsis template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package eus.tartanga.crud.logic;
