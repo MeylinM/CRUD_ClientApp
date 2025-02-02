ccfc519d9aed0bbe2f75567e6dc25d97114d929a
Irati <Irati@192.168.64.2>
2025-02-02 15:37:38 +0100

    Encryptation add to signUp and signIn (without test)

--- CRUD_ClientApp/src/eus/tartanga/crud/logic/FanetixClientRest.java
+++ CRUD_ClientApp/src/eus/tartanga/crud/logic/FanetixClientRest.java
@@ -8,9 +8,7 @@ package eus.tartanga.crud.logic;
 import eus.tartanga.crud.exception.AddException;
 import eus.tartanga.crud.exception.DeleteException;
 import eus.tartanga.crud.exception.ReadException;
-import eus.tartanga.crud.exception.SignInException;
 import eus.tartanga.crud.exception.UpdateException;
-import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
@@ -39,62 +37,152 @@ public class FanetixClientRest implements FanetixClientManager{
         webTarget = client.target(BASE_URI).path("eus.tartanga.crud.entities.fanetixclient");
     }
 
+    /**
+     *
+     * @param requestEntity
+     * @throws AddException
+     */
+    @Override
     public void createClient_XML(Object requestEntity) throws AddException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
+    /**
+     *
+     * @param requestEntity
+     * @throws AddException
+     */
+    @Override
     public void createClient_JSON(Object requestEntity) throws AddException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
     }
 
+    /**
+     *
+     * @param email
+     * @throws DeleteException
+     */
+    @Override
     public void removeClient(String email) throws DeleteException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{email})).request().delete();
     }
 
+    /**
+     *
+     * @param requestEntity
+     * @param email
+     * @throws UpdateException
+     */
+    @Override
     public void updateClient_XML(Object requestEntity, String email) throws UpdateException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{email})).request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
+    /**
+     *
+     * @param requestEntity
+     * @param email
+     * @throws UpdateException
+     */
+    @Override
     public void updateClient_JSON(Object requestEntity, String email) throws UpdateException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{email})).request(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
     }
 
+    /**
+     *
+     * @param <T>
+     * @param responseType
+     * @param email
+     * @param passwd
+     * @return
+     * @throws ReadException
+     */
+    @Override
     public <T> T signIn_XML(GenericType<T> responseType, String email, String passwd) throws ReadException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("signIn/{0}/{1}", new Object[]{email, passwd}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     *
+     * @param <T>
+     * @param responseType
+     * @param email
+     * @param passwd
+     * @return
+     * @throws ReadException
+     */
+    @Override
     public <T> T signIn_JSON(GenericType<T> responseType, String email, String passwd) throws ReadException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("signIn/{0}/{1}", new Object[]{email, passwd}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
+    /**
+     *
+     * @param <T>
+     * @param responseType
+     * @param email
+     * @return
+     * @throws ReadException
+     */
+    @Override
     public <T> T findClient_XML(GenericType<T> responseType, String email) throws ReadException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{email}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     *
+     * @param <T>
+     * @param responseType
+     * @param email
+     * @return
+     * @throws ReadException
+     */
+    @Override
     public <T> T findClient_JSON(Class<T> responseType, String email) throws ReadException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{email}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
+    /**
+     *
+     * @param <T>
+     * @param responseType
+     * @return
+     * @throws ReadException
+     */
+    @Override
     public <T> T findAllClients_XML(Class<T> responseType) throws ReadException {
         WebTarget resource = webTarget;
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     *
+     * @param <T>
+     * @param responseType
+     * @return
+     * @throws ReadException
+     */
+    @Override
     public <T> T findAllClients_JSON(Class<T> responseType) throws ReadException {
         WebTarget resource = webTarget;
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
+    /**
+     *
+     */
+    @Override
     public void close() {
         client.close();
     }
-    
+
 }
