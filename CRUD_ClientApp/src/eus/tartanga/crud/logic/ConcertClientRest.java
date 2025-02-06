4ab8af5eeaa2a735341bad30627bb5e363de5a65
Irati <Irati@192.168.64.2>
2025-02-06 21:30:17 +0100

    Concert javadoc done + exception fixed

--- CRUD_ClientApp/src/eus/tartanga/crud/logic/ConcertClientRest.java
+++ CRUD_ClientApp/src/eus/tartanga/crud/logic/ConcertClientRest.java
@@ -15,17 +15,18 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 
 /**
- * Jersey REST client generated for REST resource:ConcertFacadeREST
- * [eus.tartanga.crud.entities.concert]<br>
- * USAGE:
- * <pre>
- *        ConcertClientRest client = new ConcertClientRest();
- *        Object response = client.XXX(...);
- *        // do whatever with response
- *        client.close();
- * </pre>
+ * Cliente REST generado para interactuar con el recurso RESTful de conciertos.
  *
- * @author 2dam
+ * Esta clase permite realizar operaciones sobre los conciertos a través de un
+ * cliente REST utilizando la librería Jersey. Está diseñada para comunicarse
+ * con el servidor a través de su API y realizar operaciones como la búsqueda,
+ * creación, y actualización de conciertos.
+ *
+ * La URL base del servidor es definida por {@code BASE_URI}, que actualmente
+ * apunta a un servidor local. El cliente REST está configurado para enviar y
+ * recibir datos en formato XML y JSON según el método utilizado.
+ *
+ * @author Irati
  */
 public class ConcertClientRest implements ConcertManager {
 
@@ -33,11 +34,28 @@ public class ConcertClientRest implements ConcertManager {
     private Client client;
     private static final String BASE_URI = "http://localhost:8080/CRUD_ServerApp/api";
 
+    /**
+     * Constructor que inicializa el cliente REST y configura la URL base para
+     * acceder al recurso de conciertos.
+     *
+     * Este constructor crea un nuevo cliente REST y lo configura para
+     * interactuar con el servidor RESTful en la dirección especificada por
+     * {@code BASE_URI}. También establece el objetivo (target) en la ruta
+     * correspondiente para interactuar con el recurso de conciertos.
+     */
     public ConcertClientRest() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
         webTarget = client.target(BASE_URI).path("eus.tartanga.crud.entities.concert");
     }
 
+    /**
+     * Elimina un concierto identificado por su ID a través de una petición
+     * DELETE al servicio REST.
+     *
+     * @param id Identificador del concierto a eliminar.
+     * @throws DeleteException Si ocurre un error en la eliminación, como una
+     * respuesta de error del servidor.
+     */
     @Override
     public void removeConcert(String id) throws DeleteException {
         try {
@@ -47,6 +65,16 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Obtiene una lista de conciertos próximos en formato XML mediante una
+     * petición GET al servicio REST.
+     *
+     * @param responseType Clase del tipo de respuesta esperada.
+     * @param <T> Tipo genérico de la respuesta.
+     * @return Lista de conciertos en formato XML.
+     * @throws ReadException Si ocurre un error al recuperar los datos del
+     * servidor.
+     */
     @Override
     public <T> T findComingSoon_XML(Class<T> responseType) throws ReadException {
         WebTarget resource = webTarget;
@@ -54,6 +82,16 @@ public class ConcertClientRest implements ConcertManager {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     * Obtiene una lista de conciertos próximos en formato JSON mediante una
+     * petición GET al servicio REST.
+     *
+     * @param responseType Clase del tipo de respuesta esperada.
+     * @param <T> Tipo genérico de la respuesta.
+     * @return Lista de conciertos en formato JSON.
+     * @throws ReadException Si ocurre un error al recuperar los datos del
+     * servidor.
+     */
     @Override
     public <T> T findComingSoon_JSON(Class<T> responseType) throws ReadException {
         WebTarget resource = webTarget;
@@ -61,6 +99,16 @@ public class ConcertClientRest implements ConcertManager {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
+    /**
+     * Obtiene una lista de conciertos próximos en formato XML mediante una
+     * petición GET al servicio REST.
+     *
+     * @param responseType Clase del tipo de respuesta esperada.
+     * @param <T> Tipo genérico de la respuesta.
+     * @return Lista de conciertos en formato XML.
+     * @throws ReadException Si ocurre un error al recuperar los datos del
+     * servidor.
+     */
     @Override
     public <T> T findBetweenDates_XML(Class<T> responseType, String startDate, String endDate) throws ReadException {
         WebTarget resource = webTarget;
@@ -68,6 +116,29 @@ public class ConcertClientRest implements ConcertManager {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     * Realiza una solicitud GET a la API para obtener datos entre dos fechas
+     * especificadas.
+     *
+     * Este método realiza una llamada a un recurso RESTful utilizando el
+     * formato JSON y devuelve la respuesta en el tipo de clase especificado
+     * como parámetro de tipo genérico. Las fechas de inicio y fin se incluyen
+     * en la URL como parámetros de ruta.
+     *
+     * @param <T> El tipo de objeto que se espera como respuesta (debe ser una
+     * clase que represente la estructura de los datos).
+     * @param responseType La clase de tipo que se desea obtener como respuesta.
+     * Este parámetro indica el tipo de objeto en el que se deserializará la
+     * respuesta JSON.
+     * @param startDate La fecha de inicio en formato adecuado (por ejemplo,
+     * "yyyy-MM-dd").
+     * @param endDate La fecha de finalización en formato adecuado (por ejemplo,
+     * "yyyy-MM-dd").
+     * @return Un objeto del tipo {@code T} que contiene los datos obtenidos en
+     * formato JSON.
+     * @throws ReadException Si ocurre un error al leer los datos de la
+     * respuesta.
+     */
     @Override
     public <T> T findBetweenDates_JSON(Class<T> responseType, String startDate, String endDate) throws ReadException {
         WebTarget resource = webTarget;
@@ -75,6 +146,21 @@ public class ConcertClientRest implements ConcertManager {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
+    /**
+     * Actualiza un concierto en el servidor utilizando el formato XML.
+     *
+     * Este método realiza una solicitud PUT a un recurso RESTful para
+     * actualizar la información de un concierto en el servidor. Los datos a
+     * actualizar se pasan como un objeto que se convierte a formato XML y se
+     * envía en la solicitud. El identificador del concierto es proporcionado
+     * como un parámetro en la URL.
+     *
+     * @param requestEntity El objeto que contiene los datos actualizados del
+     * concierto. Este objeto se convertirá a formato XML antes de ser enviado.
+     * @param id El identificador único del concierto que se desea actualizar.
+     * @throws UpdateException Si ocurre un error al intentar actualizar el
+     * concierto en el servidor.
+     */
     @Override
     public void updateConcert_XML(Object requestEntity, String id) throws UpdateException {
         try {
@@ -84,6 +170,21 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Actualiza un concierto en el servidor utilizando el formato JSON.
+     *
+     * Este método realiza una solicitud PUT a un recurso RESTful para
+     * actualizar la información de un concierto en el servidor. Los datos a
+     * actualizar se pasan como un objeto que se convierte a formato JSON y se
+     * envía en la solicitud. El identificador del concierto es proporcionado
+     * como un parámetro en la URL.
+     *
+     * @param requestEntity El objeto que contiene los datos actualizados del
+     * concierto. Este objeto se convertirá a formato JSON antes de ser enviado.
+     * @param id El identificador único del concierto que se desea actualizar.
+     * @throws UpdateException Si ocurre un error al intentar actualizar el
+     * concierto en el servidor.
+     */
     @Override
     public void updateConcert_JSON(Object requestEntity, String id) throws UpdateException {
         try {
@@ -93,6 +194,27 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Realiza una búsqueda de datos en el servidor utilizando un término de
+     * búsqueda y el formato XML.
+     *
+     * Este método realiza una solicitud GET a un recurso RESTful para buscar
+     * datos relacionados con el término de búsqueda proporcionado. Los
+     * resultados se devuelven en formato XML y se deserializan en un objeto del
+     * tipo especificado por el parámetro {@code responseType}.
+     *
+     * @param <T> El tipo de objeto que se espera como respuesta (debe ser una
+     * clase que represente la estructura de los datos).
+     * @param responseType La clase de tipo que se desea obtener como respuesta.
+     * Este parámetro indica el tipo de objeto en el que se deserializará la
+     * respuesta XML.
+     * @param searchTerm El término de búsqueda que se utilizará para filtrar
+     * los datos en el servidor.
+     * @return Un objeto del tipo {@code T} que contiene los resultados de la
+     * búsqueda en formato XML.
+     * @throws ReadException Si ocurre un error al leer los datos de la
+     * respuesta.
+     */
     @Override
     public <T> T searchByTerm_XML(Class<T> responseType, String searchTerm) throws ReadException {
         WebTarget resource = webTarget;
@@ -100,6 +222,27 @@ public class ConcertClientRest implements ConcertManager {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     * Realiza una búsqueda de datos en el servidor utilizando un término de
+     * búsqueda y el formato JSON.
+     *
+     * Este método realiza una solicitud GET a un recurso RESTful para buscar
+     * datos relacionados con el término de búsqueda proporcionado. Los
+     * resultados se devuelven en formato JSON y se deserializan en un objeto
+     * del tipo especificado por el parámetro {@code responseType}.
+     *
+     * @param <T> El tipo de objeto que se espera como respuesta (debe ser una
+     * clase que represente la estructura de los datos).
+     * @param responseType La clase de tipo que se desea obtener como respuesta.
+     * Este parámetro indica el tipo de objeto en el que se deserializará la
+     * respuesta JSON.
+     * @param searchTerm El término de búsqueda que se utilizará para filtrar
+     * los datos en el servidor.
+     * @return Un objeto del tipo {@code T} que contiene los resultados de la
+     * búsqueda en formato JSON.
+     * @throws ReadException Si ocurre un error al leer los datos de la
+     * respuesta.
+     */
     @Override
     public <T> T searchByTerm_JSON(Class<T> responseType, String searchTerm) throws ReadException {
         WebTarget resource = webTarget;
@@ -107,6 +250,18 @@ public class ConcertClientRest implements ConcertManager {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
+    /**
+     * Crea un nuevo concierto en el servidor utilizando el formato XML.
+     *
+     * Este método realiza una solicitud POST a un recurso RESTful para crear un
+     * nuevo concierto. Los datos del concierto se pasan como un objeto que se
+     * convierte a formato XML y se envía en la solicitud.
+     *
+     * @param requestEntity El objeto que contiene los datos del nuevo
+     * concierto. Este objeto se convertirá a formato XML antes de ser enviado.
+     * @throws AddException Si ocurre un error al intentar crear el concierto en
+     * el servidor.
+     */
     @Override
     public void createConcert_XML(Object requestEntity) throws AddException {
         try {
@@ -116,6 +271,18 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Crea un nuevo concierto en el servidor utilizando el formato JSON.
+     *
+     * Este método realiza una solicitud POST a un recurso RESTful para crear un
+     * nuevo concierto. Los datos del concierto se pasan como un objeto que se
+     * convierte a formato JSON y se envía en la solicitud.
+     *
+     * @param requestEntity El objeto que contiene los datos del nuevo
+     * concierto. Este objeto se convertirá a formato JSON antes de ser enviado.
+     * @throws AddException Si ocurre un error al intentar crear el concierto en
+     * el servidor.
+     */
     @Override
     public void createConcert_JSON(Object requestEntity) throws AddException {
         try {
@@ -125,6 +292,26 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Obtiene un concierto específico del servidor en formato XML, dado su
+     * identificador.
+     *
+     * Este método realiza una solicitud GET a un recurso RESTful para obtener
+     * un concierto específico utilizando su identificador. Los datos se
+     * deserializan en el tipo de respuesta especificado por el parámetro
+     * {@code responseType}.
+     *
+     * @param <T> El tipo de objeto que se espera como respuesta (debe ser una
+     * clase que represente los datos del concierto).
+     * @param responseType El tipo de respuesta que se espera recibir. Este
+     * parámetro indica el tipo de objeto en el que se deserializarán los datos
+     * del concierto en formato XML.
+     * @param id El identificador único del concierto que se desea obtener.
+     * @return Un objeto del tipo {@code T} que contiene los datos del concierto
+     * obtenido en formato XML.
+     * @throws ReadException Si ocurre un error al leer los datos de la
+     * respuesta.
+     */
     @Override
     public <T> T findConcert_XML(Class<T> responseType, String id) throws ReadException {
         try {
@@ -136,6 +323,26 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Obtiene un concierto específico del servidor en formato JSON, dado su
+     * identificador.
+     *
+     * Este método realiza una solicitud GET a un recurso RESTful para obtener
+     * un concierto específico utilizando su identificador. Los datos se
+     * deserializan en el tipo de respuesta especificado por el parámetro
+     * {@code responseType}.
+     *
+     * @param <T> El tipo de objeto que se espera como respuesta (debe ser una
+     * clase que represente los datos del concierto).
+     * @param responseType El tipo de respuesta que se espera recibir. Este
+     * parámetro indica el tipo de objeto en el que se deserializarán los datos
+     * del concierto en formato JSON.
+     * @param id El identificador único del concierto que se desea obtener.
+     * @return Un objeto del tipo {@code T} que contiene los datos del concierto
+     * obtenido en formato JSON.
+     * @throws ReadException Si ocurre un error al leer los datos de la
+     * respuesta.
+     */
     @Override
     public <T> T findConcert_JSON(Class<T> responseType, String id) throws ReadException {
         try {
@@ -147,6 +354,23 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Obtiene todos los conciertos disponibles en el servidor en formato XML.
+     *
+     * Este método realiza una solicitud GET a un recurso RESTful para obtener
+     * todos los conciertos en formato XML. Los datos se deserializan en el tipo
+     * de respuesta especificado por el parámetro {@code responseType}.
+     *
+     * @param <T> El tipo de objeto que se espera como respuesta (debe ser una
+     * clase que represente una colección de conciertos).
+     * @param responseType El tipo de respuesta que se espera recibir. Este
+     * parámetro indica el tipo de objeto en el que se deserializarán los datos
+     * en formato XML.
+     * @return Un objeto del tipo {@code T} que contiene todos los conciertos
+     * obtenidos en formato XML.
+     * @throws ReadException Si ocurre un error al leer los datos de la
+     * respuesta.
+     */
     @Override
     public <T> T findAllConcerts_XML(GenericType<T> responseType) throws ReadException {
         try {
@@ -157,6 +381,23 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Obtiene todos los conciertos disponibles en el servidor en formato XML.
+     *
+     * Este método realiza una solicitud GET a un recurso RESTful para obtener
+     * todos los conciertos en formato XML. Los datos se deserializan en el tipo
+     * de respuesta especificado por el parámetro {@code responseType}.
+     *
+     * @param <T> El tipo de objeto que se espera como respuesta (debe ser una
+     * clase que represente una colección de conciertos).
+     * @param responseType El tipo de respuesta que se espera recibir. Este
+     * parámetro indica el tipo de objeto en el que se deserializarán los datos
+     * en formato XML.
+     * @return Un objeto del tipo {@code T} que contiene todos los conciertos
+     * obtenidos en formato XML.
+     * @throws ReadException Si ocurre un error al leer los datos de la
+     * respuesta.
+     */
     @Override
     public <T> T findAllConcerts_JSON(Class<T> responseType) throws ReadException {
         try {
@@ -167,6 +408,13 @@ public class ConcertClientRest implements ConcertManager {
         }
     }
 
+    /**
+     * Cierra la conexión del cliente.
+     *
+     * Este método cierra el cliente de la API, liberando los recursos asociados
+     * con él. Después de llamar a este método, el cliente ya no puede ser
+     * utilizado para realizar más solicitudes.
+     */
     @Override
     public void close() {
         client.close();
